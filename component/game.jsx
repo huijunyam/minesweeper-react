2117fa2b93bb12dc75ac724b49af0f4aeef4d743
Hui Jun <hjyam@umich.edu>
2016-12-30 18:17:33 -0800

    Add modal

--- component/game.jsx
+++ component/game.jsx
@@ -7,6 +7,7 @@ class Game extends React.Component {
     super();
     this.state = { board: new Minesweeper.Board(9, 10) };
     this.updateGame = this.updateGame.bind(this);
+    this.restartGame = this.restartGame.bind(this);
   }
 
   updateGame(tile, flagged) {
@@ -18,17 +19,27 @@ class Game extends React.Component {
     this.setState({ board: this.state.board });
   }
 
+  restartGame() {
+    const board = new Minesweeper.Board(9, 10);
+    this.setState({ board });
+  }
+
   render() {
-    let gameOverText = "";
-    if (this.state.board.won()) {
-      gameOverText = "You won!!!";
-    } else if (this.state.board.lost()) {
-      gameOverText = "You lost!!!";
+    let gameOver;
+    if (this.state.board.lost() || this.state.board.won()) {
+      const gameOverText = this.state.board.won() ? "You won!" : "You lost!";
+      gameOver =
+        <div className='modal-screen'>
+          <div className='modal-content'>
+            <p>{gameOverText}</p>
+            <button onClick={this.restartGame}>Play Again</button>
+          </div>
+        </div>;
     }
 
     return (
       <div>
-        <h1>{gameOverText}</h1>
+        {gameOver}
         <Board board={this.state.board} updateGame={this.updateGame} />
       </div>
     );
